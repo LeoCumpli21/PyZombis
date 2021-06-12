5a3e5c76b9a1e79c691d4cc21f8711c47aa7b6d7
LeoCumpli21 <LeoCumpli21@users.noreply.github.com>
2021-06-12 19:08:39 +0000

    deploy: 52671961aadc417bab55942dd2d566c9565bc546

---
+++ master/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '52671961aadc417bab55942dd2d566c9565bc546-master',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
